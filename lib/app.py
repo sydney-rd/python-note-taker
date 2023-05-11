1009ac4932657d86364ba5baa12148955aa5d222
sydney-rd <sydneydavid56@gmail.com>
2023-05-10 20:07:44 -0400

    adding function to view notes

--- lib/app.py
+++ lib/app.py
@@ -2,16 +2,20 @@ from model import *
 import argparse
 from commands import *
 
+
 def main():
     parser = argparse.ArgumentParser(description="Enter title and content of your note")
-    parser.add_argument("-t", "--title", type=str, required=True, help="Title of your note")
-    parser.add_argument("-c", "--content", type=str, required=True, help="Enter note content")
+    parser.add_argument("-t", "--title", type=str, help="Title of your note")
+    parser.add_argument("-c", "--content", type=str, help="Enter note content")
+    parser.add_argument("-v", "--view", action="store_true", help="View all notes")
     args = parser.parse_args()
-    title = args.title
-    content = args.content
-    
-    create(title, content)
 
+    if args.view:
+        view()
+    elif args.title and args.content:
+        create(args.title, args.content)
+    else:
+        parser.print_help()
 
 if __name__ == "__main__":
     main()
--- lib/commands.py
+++ lib/commands.py
@@ -1,8 +1,10 @@
 from model import *
-
+from playhouse.shortcuts import model_to_dict
 
 def create(title, content):
     note = Notes(title = title, content = content)
     note.save()
 
-
+def view():
+    for note in Notes.select():
+        print(model_to_dict(note))
